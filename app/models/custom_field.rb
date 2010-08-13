00158175bd71826f595bc5559f2027c2ee5c0b0e
rx <rx@voomify.com>
2010-08-13 11:25:51 -0400

    modified CustomField to not require a value.  It is perfectly valid to have a custom field that is empty.

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -2,8 +2,8 @@ class CustomField < ActiveRecord::Base
   belongs_to :page
 
   validates_presence_of       :name
-  validates_uniqueness_of     :name, :scope => :page_id
-  validates_presence_of       :value
+  validates_uniqueness_of     :name, :scope => :page_id  
+  #validates_presence_of       :value
   validates_presence_of       :page_id
   
   def self.find_assignable_custom_fields(page_id)
